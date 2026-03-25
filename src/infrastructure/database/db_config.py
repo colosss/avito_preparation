892c1f625f1ac5f4227f219575985bd2fd36eb11
colosss <kocnevs.jk@yandex.ru>
2026-03-25 14:26:32 +0300

    first commit

---
+++ src/infrastructure/database/db_config.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from os import getenv
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DB_PORT = getenv('DB_PORT', '5432')
+DB_HOST = getenv('DB_HOST', 'localhost')
+DB_USER = getenv('DB_USER', 'user')
+DB_PASSWORD = getenv('DB_PASSWORD', 'password')
+DB_NAME = getenv('DB_NAME', 'database')
+
+class Settings(BaseSettings):
+    db_prefix:str = "postrfresql+asyncpg"
+    db_url:str = f"{db_prefix}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+settings = Settings()
---
+++ src/infrastructure/database/db_helper.py
@@ -0,0 +1,47 @@
+from asyncio import current_task
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+    async_scoped_session,
+)
+from sqlalchemy.orm import sessionmaker
+from src.infrastructure.database.db_config import settings
+# from src.infrastructure.database.repositories.merch import MerchRepository
+# from src.infrastructure.database.repositories.user import UserRepository
+
+
+class DataBaseHelper:
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(
+            url = url,
+            echo = echo,
+        )
+        self.session_factory = async_sessionmaker(
+            bind = self.engine,
+            autoflush = False,
+            autocommit = False,
+            expire_on_commit = False,
+        )
+    
+    def get_scope_session(self):
+        session = async_scoped_session(
+            session_factory = self.session_factory,
+            scopefunc = current_task,
+        )
+        return session
+    
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+    async def scope_session_dependency(self) -> AsyncSession:
+        session = self.get_scope_session()
+        yield session
+        await session.close()
+
+db_helper = DataBaseHelper(
+    url = settings.db_url,
+    echo = settings.echo,
+)
